8e5e93795e9d6310e697b1afad98580a9fb6b492
Somdotta07 <jhilick.s@gmail.com>
2021-11-23 20:54:03 +0100

    fixed Trailing spaces

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="calculator-body">
-        <Calculator />       
+        <Calculator />
       </div>
     );
   }
