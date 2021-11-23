8edc6289952f2cd17b6dd829224582a93b4364e2
Somdotta07 <jhilick.s@gmail.com>
2021-11-23 20:51:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="calculator-body">
-        <Calculator />        
+        <Calculator />       
       </div>
-      );
+    );
   }
 }
 
