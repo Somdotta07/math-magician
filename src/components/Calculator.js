0723833b4a80336d171b7cc29585245105d35314
Somdotta07 <jhilick.s@gmail.com>
2021-11-24 16:56:47 +0100

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -33,7 +33,7 @@ class Calculator extends React.Component {
           <button type="button" id="%" className="button" onClick={() => this.handleClick('%')}>%</button>
           <button type="button" id="&divide;" className="operand-color" onClick={() => this.handleClick('÷')}>÷</button>
           <button type="button" id="7" className="button" onClick={() => this.handleClick('7')}>7</button>
-          <button type="button" id="8"className="button" onClick={() => this.handleClick('8')}>8</button>
+          <button type="button" id="8" className="button" onClick={() => this.handleClick('8')}>8</button>
           <button type="button" id="9" className="button" onClick={() => this.handleClick('9')}>9</button>
           <button type="button" id="&times;" className="operand-color" onClick={() => this.handleClick('x')}>x</button>
           <button type="button" id="4" className="button" onClick={() => this.handleClick('4')}>4</button>
@@ -44,10 +44,10 @@ class Calculator extends React.Component {
           <button type="button" id="2" className="button" onClick={() => this.handleClick('2')}>2</button>
           <button type="button" id="3" className="button" onClick={() => this.handleClick('3')}>3</button>
           <button type="button" id="+" className="operand-color" onClick={() => this.handleClick('+')}>+</button>
-          <button type="button" id="0"className="span-two" onClick={() => this.handleClick('0')}>0</button>
-          <button type="button" id="."className="button" onClick={() => this.handleClick('.')}>.</button>
+          <button type="button" id="0" className="span-two" onClick={() => this.handleClick('0')}>0</button>
+          <button type="button" id="." className="button" onClick={() => this.handleClick('.')}>.</button>
           <button type="button" id="=" className="operand-color" onClick={() => this.handleClick('=')}>=</button>
-          </div>
+        </div>
       </div>
     );
   }
